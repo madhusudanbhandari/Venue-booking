e03d8c598af2360fed23362a0bd5b195d93ba8dd
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-01-16 20:58:03 +0545

    login page creation

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/ui/login_page.dart';
-import 'package:flutter_application_1/ui/signup_page.dart';
+//import 'package:flutter_application_1/ui/signup_page.dart';
 
 void main() {
   runApp(const MyApp());
